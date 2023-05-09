957b3fe08e9e360307fd14b4282742db5ce4a449
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-05-09 18:31:09 +0100

    added error handling fn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const { default: mongoose } = require('mongoose')
+const errorHandler = require('./middlewares/errorHandler')
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -13,7 +14,7 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('hello')
 })
-
+// app.use(errorHandler)
 // CREATE SERVER AND CONNECT TO THE DB
 mongoose
   .connect(process.env.MONGO_URI)
