e22bbe98d3ee1ae67aa33485261f05e998c1e5a5
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-05-09 18:31:09 +0100

    added error handling fn

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,3 +1,10 @@
-// export const errorHandler = (req,res,err,next)=>{
+const errorHandler = (req, res, err, next) => {
+  const statusCode = res?.statusCode ? res.statusCode : 500
+  res.status(statusCode)
 
-// }
+  res.json({
+    message: err?.message,
+  })
+}
+
+module.exports = errorHandler
