3b13ce11dcdc03fa0d4971083e117f4d90973030
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-05-24 11:25:13 +0100

    created email sending function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,53 +1,53 @@
-const asyncHandler = require("express-async-handler");
-const path = require("path");
-const User = require("../models/userModel");
+const asyncHandler = require('express-async-handler')
+const path = require('path')
+const User = require('../models/userModel')
 
 // REGISTER USER
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password } = req.body
 
   // CHECK IF NAME, EMAIL, PASSWORD ARE PRESENT
   if (!name || !email || !password) {
-    res.status(400);
-    throw new Error("Pleae fill in the required info");
+    res.status(400)
+    throw new Error('Pleae fill in the required info')
   }
 
   // CHECK IF PASSWORD'S LENGTH IS GREATER THAN 4
   if (password?.length < 4) {
-    res.status(400);
-    throw new Error("Password can not be less that four(4) letters");
+    res.status(400)
+    throw new Error('Password can not be less that four(4) letters')
   }
 
   // CHECK IF USER ALREADY EXISTS
-  const userEmail = await User.findOne({ email: email });
+  const userEmail = await User.findOne({ email: email })
   if (userEmail) {
-    res.status(400);
-    throw new Error("User already exists");
+    res.status(400)
+    throw new Error('User already exists')
   }
 
-  // IF IT PASSES ALL THE ABOVE CONDITIONS, SAVE USER
+  // IF IT PASSES ALL THE ABOVE CONDITIONS, SEND EMAIL CONFIRMATION LINK
 
-  const fileName = req.file?.filename;
-  const fileUrl = fileName ? path?.join(fileName) : null;
+  const fileName = req.file?.filename
+  const fileUrl = fileName ? path?.join(fileName) : null
 
   const user = {
     name,
     email,
     password,
     // avatar: fileUrl,
-  };
+  }
 
-  const newUser = await User.create(user);
+  const newUser = await User.create(user)
 
   if (newUser) {
-    const { password, ...others } = newUser?._doc;
-    res.status(200).json(others);
+    const { password, ...others } = newUser?._doc
+    res.status(200).json(others)
   } else {
-    res.status(500);
-    throw new Error("Something went wrong!");
+    res.status(500)
+    throw new Error('Something went wrong!')
   }
-});
+})
 
 module.exports = {
   registerUser,
-};
+}
