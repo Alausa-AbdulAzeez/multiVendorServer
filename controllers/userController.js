d895b4b39249913929357af6d942c46860e9b3a2
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-05-28 02:03:37 +0100

    cookies now being sent to postman

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,42 +1,42 @@
-const asyncHandler = require('express-async-handler')
-const path = require('path')
-const User = require('../models/userModel')
-const jwt = require('jsonwebtoken')
-const { Router } = require('express')
-const sendMail = require('../miscellaneous/sendMail')
+const asyncHandler = require("express-async-handler");
+const path = require("path");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const { Router } = require("express");
+const sendMail = require("../miscellaneous/sendMail");
 
 // GENERATE TOKEN
 const generateToken = (user) => {
-  return jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: '5m' })
-}
+  return jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: "1d" });
+};
 // REGISTER USER
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body
+  const { name, email, password } = req.body;
 
   // CHECK IF NAME, EMAIL, PASSWORD ARE PRESENT
   if (!name || !email || !password) {
-    res.status(400)
-    throw new Error('Pleae fill in the required info')
+    res.status(400);
+    throw new Error("Pleae fill in the required info");
   }
 
   // CHECK IF PASSWORD'S LENGTH IS GREATER THAN 4
   if (password?.length < 4) {
-    res.status(400)
-    throw new Error('Password can not be less that four(4) letters')
+    res.status(400);
+    throw new Error("Password can not be less that four(4) letters");
   }
 
   // CHECK IF USER ALREADY EXISTS
-  const userEmail = await User.findOne({ email: email })
+  const userEmail = await User.findOne({ email: email });
   if (userEmail) {
-    res.status(400)
-    throw new Error('User already exists')
+    res.status(400);
+    throw new Error("User already exists");
   }
 
   // IF IT PASSES ALL THE ABOVE CONDITIONS, SEND EMAIL CONFIRMATION LINK
   // IF IT PASSES ALL THE ABOVE CONDITIONS, CREATE JWT
 
-  const fileName = req.file?.filename
-  const fileUrl = fileName ? path?.join(fileName) : null
+  const fileName = req.file?.filename;
+  const fileUrl = fileName ? path?.join(fileName) : null;
 
   //  CREATE NEW USER
   const user = {
@@ -44,17 +44,17 @@ const registerUser = asyncHandler(async (req, res) => {
     email,
     password,
     // avatar: fileUrl,
-  }
+  };
 
   // GENERATE TOKEN
-  const token = generateToken(user)
+  const token = generateToken(user);
 
   // ACTIVATION URL
-  const activationUrl = `http://localhost:3000/activate/${token}`
+  const activationUrl = `http://localhost:3000/activate/${token}`;
 
   await sendMail({
     email: user.email,
-    subject: 'Activate your account',
+    subject: "Activate your account",
     message: `Hello ${user.name}, please click on the link to activate your account: ${activationUrl}`,
   })
     .then(() =>
@@ -63,64 +63,67 @@ const registerUser = asyncHandler(async (req, res) => {
       })
     )
     .catch((error) => {
-      res.status(500)
-      throw new Error(error)
-    })
-})
+      res.status(500);
+      throw new Error(error);
+    });
+});
 
 // ACTIVATE USER
 const activateUser = asyncHandler(async (req, res) => {
-  const { activation_token } = req.body
+  const { activation_token } = req.body;
 
   // OPTIONS FOR COOKIES
   const options = {
+    path: "/",
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-    httpOnly: true,
-    sameSite: 'none',
-    secure: true,
-  }
+    httpOnly: false,
+    sameSite: "none",
+    secure: false,
+  };
 
   // CHECK IF THE ACTIVATION TOKEN IS PRESENT IN THE BODY
   if (!activation_token) {
-    res.status(400)
+    res.status(400);
     throw new Error(
-      'Please send your token. Please send the token in this format {activation_token:token}'
-    )
+      "Please send your token. Please send the token in this format {activation_token:token}"
+    );
   }
 
   // CHECK IF TOKEN IS VALID
-  const user = jwt.verify(activation_token, process.env.JWT_SECRET)
+  const user = jwt.verify(activation_token, process.env.JWT_SECRET);
 
   // THROW ERROR FOR AN INVALID TOKEN
   if (!user) {
-    res.status(400)
-    throw new Error('Invalid Token')
+    res.status(400);
+    throw new Error("Invalid Token");
   }
 
   // CHECK IF USER EXISTS ALREADY
-  const { email } = user?.user
+  const { email } = user?.user;
 
-  const userExists = await User.findOne({ email })
+  const userExists = await User.findOne({ email });
 
   if (userExists) {
-    res.status(400)
-    throw new Error('User already exists')
+    res.status(400);
+    throw new Error("User already exists");
   }
+  const token = activation_token;
+  res.status(200).cookie("token", token, options).json("Check cookie");
 
   // CREATE USER
-  const newUser = await User.create(user?.user)
-  if (newUser) {
-    // SEND COOKIE
-    res.cookie('token', activation_token, options)
-    const { password, ...others } = newUser?._doc
-    res.status(200).json({ ...others })
-  } else {
-    res.status(500)
-    throw new Error('Something went wrong!')
-  }
-})
+  // const newUser = await User.create(user?.user);
+  // if (newUser) {
+  //   // SEND COOKIE
+  //   res.cookie("token", activation_token, options);
+  //   const { password, ...others } = newUser?._doc;
+  //   res.status(200).json({ ...others });
+  // } else {
+  //   res.status(500);
+  //   throw new Error("Something went wrong!");
+  // }
+});
 
 module.exports = {
   registerUser,
   activateUser,
-}
+};
--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const { default: mongoose } = require('mongoose')
-const errorHandler = require('./middlewares/errorHandler')
-const userRoutes = require('./routes/userRoutes')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
+const errorHandler = require("./middlewares/errorHandler");
+const userRoutes = require("./routes/userRoutes");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
-const app = express()
-const PORT = process.env.PORT || 5000
-dotenv.config()
+const app = express();
+const PORT = process.env.PORT || 5000;
+dotenv.config();
 
 // MIDDLEWARES
-app.use(cors())
-app.use(express.json())
-app.use(cookieParser())
-app.use('/', express.static('uploads'))
-app.use(bodyParser.json())
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+    allowedHeaders: "*",
+    withCredentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use("/", express.static("uploads"));
+app.use(bodyParser.json());
 
 // ROUTES
-app.use('/api/user', userRoutes)
+app.use("/api/user", userRoutes);
 
-app.use(errorHandler)
+app.use(errorHandler);
 // CREATE SERVER AND CONNECT TO THE DB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`)
-    })
+      console.log(`Server running on port ${PORT}`);
+    });
   })
   .catch((error) => {
-    console.log(error)
-  })
+    console.log(error);
+  });
